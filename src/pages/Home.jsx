51becef45569a803006fcaee0af7a281b7dc68bc
aswinnt <aswinmalamakkavu@gmail.com>
2026-02-06 14:09:32 +0530

    Initial React frontend setup with Vite

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Find My Seat 🎟️</h1>
+      <p>Discover events. Choose seats. Book instantly.</p>
+    </div>
+  );
+}
+
+export default Home;
